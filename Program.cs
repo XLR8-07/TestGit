fd1655bbe4626def732b387cbbdf1c3b17f914b4
XLR8-07 <ashiqurrahman85@iut-dhaka.edu>
2021-02-02 04:09:57 +0600

    Roll number jigaisi

--- Program.cs
+++ Program.cs
@@ -10,9 +10,12 @@ namespace TestProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("TOR NAM KI? ");
+            Console.WriteLine("TOR NAM KI: ");
             string name = Console.ReadLine();
             Console.WriteLine("HEHE MONU, MOR NAM " + name);
+            Console.WriteLine("DADA, TOMAR ROLL KOTO: ");
+            string roll = Console.ReadLine();
+            Console.WriteLine("MOR ROLL hoilo :" + roll);
             Console.ReadKey();
         }
     }
