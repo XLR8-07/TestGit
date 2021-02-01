4265c34dca7f32aaaa9ffc23dadb3fd943e8d185
XLR8-07 <ashiqurrahman85@iut-dhaka.edu>
2021-02-02 04:01:22 +0600

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("TOR NAM KI? ");
+            string name = Console.ReadLine();
+            Console.WriteLine("HEHE MONU, MOR NAM " + name);
+            Console.ReadKey();
+        }
+    }
+}
